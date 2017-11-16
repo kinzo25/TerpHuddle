2e7e1028567db1a66e65be7fc41b45202901df6e
mgiezeman <31415714+mgiezeman@users.noreply.github.com>
2017-11-15 20:36:47 -0500

    Update new_user_form.php

--- new_user_form.php
+++ new_user_form.php
@@ -2,10 +2,10 @@
 <head>
 <meta content="text/html; charset=ISO-8859-1"
 http-equiv="content-type">
-<title>New Employee</title>
+<title>New User</title>
 </head>
 <body>
- <h2>Enter a New Employee</h2>
+ <h2>Enter New User Information</h2>
   <?php
 $database=mysqli_connect('localhost', 'mgiezema', 'mgiezema01','g1');
 ?>
@@ -13,10 +13,10 @@ $database=mysqli_connect('localhost', 'mgiezema', 'mgiezema01','g1');
 		action = "http://g1.psjconsulting.com/new_user.php"
 		>
 
-First Name: <input type="text" name="first_name"> <br><br>
-Last Name: <input type="text" name="last_name"> <br><br>
-Email: <input type="text" name="email"> <br><br>
-Phone Number: <input type="text" name="phone"> <br><br>
+First Name: <input type="text" name="first_name" required pattern="[A-Za-z' -]+" title="Only letters, spaces, apostrophes, and hyphens are allowed."> <br><br>
+Last Name: <input type="text" name="last_name" required pattern="[A-Za-z' -]+" title="Only letters, spaces, apostrophes, and hyphens are allowed."> <br><br>
+Email: <input type="email" name="email" required> <br><br>
+Phone Number: <input type="tel" name="phone" required pattern="[0-9]{3}[ -][0-9]{3}[ -][0-9]{4}" title="Use format 000-000-0000"> <br><br>
 
 <br><br>
 <input type="submit" name="submit">
